c4c5bf7c093b08f705636775587ec2701e0e3ded
wagbitconsulting <sean@wagbit.com>
2024-08-20 17:18:38 -0500

    Commits After Completion of Labs

--- giftlink-frontend/src/index.js
+++ giftlink-frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+// import reportWebVitals from './reportWebVitals';
 import {AuthProvider} from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,3 +17,4 @@ root.render(
   </React.StrictMode>
 );
 
+// reportWebVitals();
